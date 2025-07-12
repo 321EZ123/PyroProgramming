33e53508ac8106b40a2e8fd79d5b71842190d8eb
321EZ123 <139788167+321EZ123@users.noreply.github.com>
2025-07-12 10:45:42 -0400

    stuff

--- src/app/tutorials/css/page.tsx
+++ src/app/tutorials/css/page.tsx
@@ -57,7 +57,11 @@ p {
             </pre>
           </div>
           <div className="bg-card p-3 flex justify-center">
-            <Button variant="default" className="bg-primary hover:bg-primary/90 text-white">
+            <Button 
+              variant="default" 
+              className="bg-primary hover:bg-primary/90 text-white"
+              onClick={() => window.location.href = "https://pyroprogramming-ide.9p.al/"}
+            >
               Try it Yourself
             </Button>
           </div>
